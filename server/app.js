f1ed5aa9e807db6f22e16b6247986fb844a31777
Matthew Sze-Tu <ms1937@rit.edu>
2021-10-27 00:47:29 -0400

    Maker B done

--- server/app.js
+++ server/app.js
@@ -5,30 +5,37 @@ const favicon = require('serve-favicon');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
 
 mongoose.connect(dbURL, (err) => {
-    if (err) {
-        console.log('Could not connect to database');
-        throw err;
-    }
+  if (err) {
+    console.log('Could not connect to database');
+    throw err;
+  }
 });
 
-//pull in our routes
-const router = require('./router.js');
+// pull in our routes
 const bodyParser = require('body-parser');
+const router = require('./router.js');
 
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
-    extended: true,
+  extended: true,
 }));
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.use(session({
+  key: 'sessionid',
+  secret: 'Domo Arigato',
+  resave: true,
+  saveUninitialized: true,
+}));
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
@@ -36,8 +43,8 @@ app.use(cookieParser());
 router(app);
 
 app.listen(port, (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log(`Listening on port ${port}`);
+  if (err) {
+    throw err;
+  }
+  console.log(`Listening on port ${port}`);
 });
